7fe29f8d99024d53db91ebb67919817ae6a10a72
Nabeel Shahzad <nshahzad@gmail.com>
2012-08-17 15:32:49 -0400

    set() to add vars; refactoring

--- sample/json.php
+++ sample/json.php
@@ -9,22 +9,45 @@ include THIS_PATH . '/../src/Codon/SuperObj.php';
 include THIS_PATH . '/../src/Codon/Config.php';
 
 $instance = \Codon\Config::loadDataJSON(file_get_contents(THIS_PATH . '/../tests/test.json'));
-#$value = $instance->get('testApplication', 'configuration', 'database', 'host');
-#var_dump($value);
 
+echo "==== string values ======\n";
 echo "=========================\n";
+
+$value = $instance->get('testApplication', 'configuration', 'database', 'host');
+echo 'DB_HOST_VALUE: '; var_dump($value);
+
+$value = $instance->get('testApplication.id');
+echo 'testApplication.id: '; var_dump($value);
+
+echo "==== array values =======\n";
+echo "=========================\n";
+
+$cdn = $instance->get(['testApplication', 'cdn']);
+echo 'RETURNED VALUES: '; var_dump($cdn);
+
+/*$value = $instance->get(['testApplication', 'cdn.0']);
+echo 'SHOULD BE NULL RETURNED VALUES: '; var_dump($value);*/
+
+$value = $instance->get('testApplication.systems');
+echo '(MAP) RETURNED VALUES: '; var_dump($value);
+
+echo "==== set values=======\n";
+echo "=========================\n";
+
+# Set new
+echo 'BEFORE SET: '; var_dump($instance->get('test.value'));
+$instance->set('test.value', 'hello');
+echo 'AFTER SET: '; var_dump($instance->get('test.value'));
+
+$instance->set('test.array', ['oranges', 'bananas']);
+echo 'SET ARRAY: '; var_dump($instance->get('test.array'));
+
+echo 'GET TEST NODE: '; var_dump($instance->get('test'));
+
+#$instance->set('test.Application.cdn', array_push($cdn, $cdn));
+
 echo "==== array values =======\n";
 echo "=========================\n";
 
 $value = $instance->get(['testApplication', 'cdn']);
 echo 'RETURNED VALUES: '; var_dump($value);
-
-#$value = $instance->get(['testApplication', 'cdn.0']);
-#echo 'RETURNED VALUES: '; var_dump($value);
-
-$value = $instance->get(['testApplication', 'systems']);
-echo 'RETURNED VALUES: '; var_dump($value);
-
-
-#$value = \Codon\Config::getPath('testApplication.configuration.database.host');
-#var_dump($value);
--- src/Codon/SuperObj.php
+++ src/Codon/SuperObj.php
@@ -36,11 +36,6 @@
 
 namespace Codon;
 
-/**
- * @TODO
- *
- * Parse testApplications.systems
- */
 class SuperObj extends \ArrayObject {
 
 	protected $_cache = [];
@@ -61,11 +56,42 @@ class SuperObj extends \ArrayObject {
 
 
 	/**
-	 * @param $path
-	 * @param $value
+	 * Save values/set them to an arbitrary $path
+	 * @param string $path Path to save (x.y.z)
+	 * @param mixed $value Value for the above path
 	 */
 	public function set($path, $value) {
 
+		$path_list = explode('.', $path);
+
+		# See if this is a node already exists
+		list($node, $node_value) = $this->findNode($this->getIterator(), $path_list);
+		if($node !== null) {
+			$node->offsetSet($node->key(), $value);
+			$this->saveCache($path, $value);
+			return;
+		}
+
+		# See if the first index exists; if not, create it and assign it
+		# Then set a parent tree to that index. Then just crawl up the nodes
+		# Creating each level
+		$p = array_shift($path_list);
+		if(!isset($this[$p])) {
+			$this[$p] = [];
+		}
+
+		$parent_key = &$this[$p];
+		foreach($path_list as $p) {
+			# create new node, if it doesn't exist
+			if(!isset($parent_key[$p])) {
+				$parent_key[$p] = [];
+			}
+			$parent_key = &$parent_key[$p];
+		}
+
+		# Finally set the value because we're at the end
+		$parent_key = $value;
+		$this->saveCache($path, $value);
 	}
 
 
@@ -74,13 +100,14 @@ class SuperObj extends \ArrayObject {
 	 * @param string $map The path to return
 	 * @return mixed
 	 */
-	public function getPath($map) {
+	public function getByPath($map) {
 		return $this->get($map);
 	}
 
 
     /**
-     * Pass in the path
+     * Pass in the path, or a list with the path of the key you want
+	 * Returns the value from that path
 	 * @return mixed
      */
     public function get() {
@@ -88,81 +115,137 @@ class SuperObj extends \ArrayObject {
 		$tree = func_get_args();
 		$count = func_num_args();
 		if($count === 1) {
-			if(is_array($tree[0]))  # They passed in an array
+			# They passed in an array, so assume this is our list
+			if(is_array($tree[0])) {
 				$tree = $tree[0];
-			elseif(is_string($tree[0])) # Assume they passed in a path
+			}
+			# Passed in a string, assume this is a path
+			elseif(is_string($tree[0])) {# Assume they passed in a path
 				$tree = explode($this->_pathDelim, $tree[0]);
+			}
 		}
 
-        # Store the name reversed, it'll be faster to find among like-keys
-		# This is mostly useful for doing a ton of token-replacements which
-		# are found as single keys, but also for repeated-access
-        $cached_name = strrev(implode('', $tree));
-        if(isset($this->_cache[$cached_name]))
-            return $this->_cache[$cached_name];
+		$tree_key = implode('.', $tree);
+
+		# Check to see if we have a copy of this key cached already
+        $value = $this->getCached($tree_key);
+		if($value !== null)
+			return $value;
 
 		$value = $this->findValue($this->getIterator(), $tree);
 
-		$this->_cache[$cached_name] = $value;
+		# Save it to the cache
+		$this->saveCache($tree_key, $value);
 
         return $value;
     }
 
 
 	/**
-	 * Returns an iterator of the current position
+	 * Return a cached copy of a given key string. Stores the key name
+	 * as a reversed string, for a faster lookup time (esp for nested-sets)
+	 * @param string $key_name
+	 * @return mixed
+	 */
+	public function getCached($key_name) {
+
+		$key_name = strrev($key_name);
+
+		if(isset($this->_cache[$key_name])) {
+			return $this->_cache[$key_name];
+		}
+
+		return null;
+	}
+
+
+	/**
+	 * Save a copy of the key to the cache
+	 * @param $key_name
+	 * @param $value
+	 */
+	public function saveCache($key_name, $value) {
+		$key_name = strrev($key_name);
+		$this->_cache[$key_name] = $value;
+	}
+
+
+	/**
+	 * Find and return the bottom node in the given tree
 	 * @param \RecursiveArrayIterator $iterator
 	 * @param array $tree
-	 * @return \RecursiveArrayIterator|null
+	 * @return mixed Array with 0: iterator, 1: value (reference)
 	 */
-	protected function findValue(\RecursiveArrayIterator &$iterator, $tree = []) {
+	protected function findNode(\RecursiveArrayIterator &$iterator, $tree = []) {
 
-        $find_key = array_shift($tree);
+		$find_key = array_shift($tree);
 
-        foreach($iterator as $key => &$value) {
-            if($key === $find_key) {
+		foreach($iterator as $key => &$value) {
 
-				# We've gotten down to the node that we want ($tree is now null)
-                if(!isset($tree[0])) {
+			if($key !== $find_key) { continue; }
 
-					# The node we want has children, parse through the leafs
-					# and parse any tokens that are present in them
-                    if($iterator->hasChildren()) {
+			# $tree isn't null yet, meaning we still have to travel down
+			# nodes in order to get to the last one... inception
+			if(isset($tree[0])) {
+				return $this->findNode($iterator->getChildren(), $tree);
+			}
 
-						$rec_ii = new \RecursiveIteratorIterator (
-							$iterator->getChildren(),
-							\RecursiveIteratorIterator::LEAVES_ONLY
-						);
+			# Return a reference to this current node - it's needed later. More details
+			# are in the findValue() function. Yeah, it's kinda hackey
+			return [$iterator, &$value];
+		}
 
-						iterator_apply($rec_ii, function(\Iterator &$iterator, &$val){
+		return null;
+	}
 
-							$curr_key = $iterator->key();
-							$curr_val = $this->parseTokens($iterator->current());
 
-							$iterator->offsetSet($curr_key, $curr_val);
+	/**
+	 * Returns an iterator of the current position
+	 * @param \RecursiveArrayIterator $node
+	 * @param array $tree
+	 * @return mixed|null
+	 */
+	protected function findValue(\RecursiveArrayIterator &$node, $tree = []) {
 
-							# This is a bug in PHP or something - if $val is an array of strings, then the
-							# offsetSet() doesn't "stick" the new values, so this is a work-around
-							if(isset($val[$curr_key])) {
-								$val[$curr_key] = $curr_val;
-							}
+		# Find the node we are looking for, and return the iterator and value
+		list($node, $node_value) = $this->findNode($node, $tree);
 
-							return true;
-						}, [&$rec_ii, &$value]);
+		if($node_value === null) { return null; }
 
-					} else {
-						$value = $this->parseTokens($value);
-					}
+		# This node has no children, it's just a string value, replace and return
+		if($node->hasChildren() === false) {
+			$node->offsetSet($node->key(), $this->parseTokens($node->current()));
+			return $node->current();
+		}
 
-					return $value;
-                }
+		# The node we want has children, parse through the
+		# leafs and parse any tokens that are present in them
+		$rec_ii = new \RecursiveIteratorIterator (
+			$node->getChildren(),
+			\RecursiveIteratorIterator::LEAVES_ONLY
+		);
 
-                return $this->findValue($iterator->getChildren(), $tree);
-            }
-        }
+		iterator_apply($rec_ii, function(\Iterator &$iter, &$node_value){
 
-        return null;
+			$curr_key = $iter->key();
+			$curr_val = $this->parseTokens($iter->current());
+
+			$iter->offsetSet($curr_key, $curr_val);
+
+			# This is a bug in PHP or something - if $nodeValue is an array, then the
+			# offsetSet() doesn't "stick" the new values, since they're not passed
+			# into the iterator by reference. So this is a work-around
+			if(isset($node_value[$curr_key])) {
+				$node_value[$curr_key] = $curr_val;
+			}
+
+			return true;
+
+		}, [&$rec_ii, &$node_value]);
+
+		return $node_value;
     }
+
 
 	/**
 	 * Find and replace all tokens in a string
@@ -172,21 +255,27 @@ class SuperObj extends \ArrayObject {
 	public function parseTokens($input) {
 
 		$tokens = $this->findTokens($input);
-		if($tokens !== false) {
-			$tokens_mappings = [];
-			foreach($tokens as $t) {
+		if($tokens === false) { return $input; }
 
-				# If a token hasns't been found, then leave it intact, don't replace it
-				$val = $this->get($t);
-				if($val !== null) {
-					$tokens_mappings[$t] = $val;
-				}
+		# Blank, to only replace tokens that are
+		foreach($tokens as $t) {
+
+			# Look for it in the mapping list/cache, if not found, then retrieve it
+			if(isset($this->_tokenMappings[$t])) { continue; }
+
+			$token_val = $this->getCached($t);
+			if($token_val === null) {
+				$token_val = $this->findValue($this->getIterator(), explode('.', $t));
+				$this->saveCache($t, $token_val);
 			}
 
-			$input = $this->replaceTokens($tokens_mappings, $input);
+			# If we found a value for it, then add it to the replacement table
+			if($token_val !== null) {
+				$this->_tokenMappings[$t] = $token_val;
+			}
 		}
 
-		return $input;
+		return $this->replaceTokens($this->_tokenMappings, $input);
 	}
 
 
@@ -208,8 +297,7 @@ class SuperObj extends \ArrayObject {
 			$matches[] = substr($string, $start + 2, ($end - $start - 2));
 			$start = strpos($string, $this->_tokenStart, $end);
 
-			if($start === false)
-				break;
+			if($start === false) { break; }
 		}
 
 		return $matches;
